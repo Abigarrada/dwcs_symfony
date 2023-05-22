fef3de86adfb97058d14a21ee0f0cfee02457aab
Abigarrada <lettieprieto@gmail.com>
2023-05-22 21:41:18 +0200

    exercicios ud08 completos, erro solucionado

--- symfony_projects/src/Controller/TiendaController.php
+++ symfony_projects/src/Controller/TiendaController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Controller;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\CacheItemInterface;
+//use Symfony\Contracts\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemInterface;
 
 class TiendaController extends AbstractController
 {
